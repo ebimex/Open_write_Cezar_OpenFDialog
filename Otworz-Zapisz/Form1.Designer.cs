6ac9f5d70de085671771f6f4e2e0ece82e1ab31f
ebimex <ebimex65@gmail.com>
2022-05-21 23:16:47 +0200

    first commit

---
+++ Otworz-Zapisz/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Otworz_Zapisz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxSciezka = new System.Windows.Forms.TextBox();
+            this.textBoxZapis = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOdczyt = new System.Windows.Forms.Button();
+            this.btnZapisz = new System.Windows.Forms.Button();
+            this.btnCzysc = new System.Windows.Forms.Button();
+            this.btnZamknij = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonSzukaj = new System.Windows.Forms.Button();
+            this.textBoxSzukaj = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxSciezka
+            // 
+            this.textBoxSciezka.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSciezka.Location = new System.Drawing.Point(27, 30);
+            this.textBoxSciezka.Name = "textBoxSciezka";
+            this.textBoxSciezka.ReadOnly = true;
+            this.textBoxSciezka.Size = new System.Drawing.Size(846, 20);
+            this.textBoxSciezka.TabIndex = 0;
+            // 
+            // textBoxZapis
+            // 
+            this.textBoxZapis.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxZapis.Location = new System.Drawing.Point(27, 370);
+            this.textBoxZapis.Multiline = true;
+            this.textBoxZapis.Name = "textBoxZapis";
+            this.textBoxZapis.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxZapis.Size = new System.Drawing.Size(846, 223);
+            this.textBoxZapis.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ścieżka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(28, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tekst do zaszyfrowania i zapisu do pliku";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.251F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(24, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(315, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Odszyfrowany tekst \"SZYREM CEZARA\" z otwatergo pliku";
+            // 
+            // btnOdczyt
+            // 
+            this.btnOdczyt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnOdczyt.Location = new System.Drawing.Point(27, 618);
+            this.btnOdczyt.Name = "btnOdczyt";
+            this.btnOdczyt.Size = new System.Drawing.Size(83, 38);
+            this.btnOdczyt.TabIndex = 6;
+            this.btnOdczyt.Text = "Odczytaj";
+            this.btnOdczyt.UseVisualStyleBackColor = true;
+            this.btnOdczyt.Click += new System.EventHandler(this.btnOdczyt_Click);
+            // 
+            // btnZapisz
+            // 
+            this.btnZapisz.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnZapisz.Location = new System.Drawing.Point(116, 618);
+            this.btnZapisz.Name = "btnZapisz";
+            this.btnZapisz.Size = new System.Drawing.Size(83, 38);
+            this.btnZapisz.TabIndex = 7;
+            this.btnZapisz.Text = "Zapisz";
+            this.btnZapisz.UseVisualStyleBackColor = true;
+            this.btnZapisz.Click += new System.EventHandler(this.btnZapisz_Click);
+            // 
+            // btnCzysc
+            // 
+            this.btnCzysc.Location = new System.Drawing.Point(217, 618);
+            this.btnCzysc.Name = "btnCzysc";
+            this.btnCzysc.Size = new System.Drawing.Size(83, 38);
+            this.btnCzysc.TabIndex = 8;
+            this.btnCzysc.Text = "Wyczyść";
+            this.btnCzysc.UseVisualStyleBackColor = true;
+            this.btnCzysc.Click += new System.EventHandler(this.btnCzysc_Click);
+            // 
+            // btnZamknij
+            // 
+            this.btnZamknij.Location = new System.Drawing.Point(775, 618);
+            this.btnZamknij.Name = "btnZamknij";
+            this.btnZamknij.Size = new System.Drawing.Size(83, 38);
+            this.btnZamknij.TabIndex = 9;
+            this.btnZamknij.Text = "Zamknij";
+            this.btnZamknij.UseVisualStyleBackColor = true;
+            this.btnZamknij.Click += new System.EventHandler(this.btnZamknij_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(595, 626);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 30);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(408, 631);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Przesunięcie tekstu";
+            // 
+            // buttonSzukaj
+            // 
+            this.buttonSzukaj.Location = new System.Drawing.Point(388, 67);
+            this.buttonSzukaj.Name = "buttonSzukaj";
+            this.buttonSzukaj.Size = new System.Drawing.Size(93, 23);
+            this.buttonSzukaj.TabIndex = 12;
+            this.buttonSzukaj.Text = "Szukaj";
+            this.buttonSzukaj.UseVisualStyleBackColor = true;
+            this.buttonSzukaj.Click += new System.EventHandler(this.buttonSzukaj_Click);
+            // 
+            // textBoxSzukaj
+            // 
+            this.textBoxSzukaj.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSzukaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxSzukaj.Location = new System.Drawing.Point(487, 67);
+            this.textBoxSzukaj.Name = "textBoxSzukaj";
+            this.textBoxSzukaj.Size = new System.Drawing.Size(371, 23);
+            this.textBoxSzukaj.TabIndex = 13;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 96);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(846, 235);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 679);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBoxSzukaj);
+            this.Controls.Add(this.buttonSzukaj);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnZamknij);
+            this.Controls.Add(this.btnCzysc);
+            this.Controls.Add(this.btnZapisz);
+            this.Controls.Add(this.btnOdczyt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxZapis);
+            this.Controls.Add(this.textBoxSciezka);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1375, 707);
+            this.MinimumSize = new System.Drawing.Size(897, 707);
+            this.Name = "Form1";
+            this.Text = "Formularz do szyfrowania KODEM CEZARA tekstu i zapisywania / otwiernai pliku";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSciezka;
+        private System.Windows.Forms.TextBox textBoxZapis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOdczyt;
+        private System.Windows.Forms.Button btnZapisz;
+        private System.Windows.Forms.Button btnCzysc;
+        private System.Windows.Forms.Button btnZamknij;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonSzukaj;
+        private System.Windows.Forms.TextBox textBoxSzukaj;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Otworz-Zapisz/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Diagnostics;
+
+
+
+
+namespace Otworz_Zapisz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //na początek zablokuj textboxy do wpisywania do czasu wpisania liczby persunięcia tekstu
+            if (textBox1.Text == "")
+            {
+                btnOdczyt.Enabled = false;
+                btnZapisz.Enabled = false;
+            }
+        }
+
+
+
+        //zmienna 'pobierz' do ktotej konwertowana jest liczba z okna do wpisywania o ile przesunać tekst
+        int pobierz()
+        {
+            return Convert.ToInt32(textBox1.Text);
+        }
+
+
+        
+
+        private void btnZapisz_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxZapis.Text))                       //jeżeli nie ma nic do zapisania z textboxa
+            {
+                MessageBox.Show("Wprowadź tekst do zapisu.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "InfoFile |*.eb";
+
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)            //jeżeli jest coś do zapisania pokaże się okno oraz potwiedzenie OK
+            {
+                textBoxSciezka.Text = dialog.FileName;                                  // wpisz do textboksa ścieżkę pliku
+
+                string ZmiennaTextDoSzyfrowania = textBoxZapis.Text;                    //z txtboxa do zapisu pobierz tekst do zmiennej string
+
+                var chars = ZmiennaTextDoSzyfrowania.ToCharArray();                     //przekonertyuj ten tekst do tablicy
+
+                string gotowe = "";                                                     //utwórz pustą zmienną 'gotowe'
+
+                for (int clk = 0; clk < chars.Length; clk++)
+                {
+                    int ascii = Convert.ToInt32(chars[clk]);
+                    ascii += pobierz();
+                    char c = Convert.ToChar(ascii);
+                    gotowe += c;                                                        //po zaszyfrowaniu wrzucaj chary do zmiennej string 'gotowe'
+                }
+
+                //File.WriteAllText(dialog.FileName, gotowe);
+                File.WriteAllText(dialog.FileName, gotowe, Encoding.Default);
+
+                MessageBox.Show("Tekst zapisany.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                return;
+        }
+
+
+
+
+        // wyświetlenie okna „Otwórz” potrzebnna jest kontrolka 'FileDialog' z przybornika
+        private void btnOdczyt_Click(object sender, EventArgs e)        
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "InfoFile |*.eb";
+
+
+            //deszyfrowanie tekstu KODEM CEZARA Z PLIKU TEKSTOWEGO
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBoxSciezka.Text = dialog.FileName;                                      // wpisz do textboksa ścieżkę pliku
+
+                var tekst =File.ReadAllLines(dialog.FileName);                              //przepisz wszytkie linie z pliku do zmiennej
+
+                textBoxZapis.Text = File.ReadAllText((dialog.FileName), Encoding.Default);
+
+                string ZmiennaTextDoDeszyfrowania = File.ReadAllText((dialog.FileName), Encoding.Default);       //przepisz dalej z zmiennej var do zmiennej string
+
+                var chars = ZmiennaTextDoDeszyfrowania.ToCharArray();                       //dalej przepisz te dane do do tablicy i umieśc je w tablicy o nazwie 'chars'
+
+                string gotowe = "";                                                         //utwórz jeszcze pusty strin o nazwie 'gotowe'
+                for (int clk = 0; clk < chars.Length; clk++)                               
+                {
+                    int ascii = Convert.ToInt32(chars[clk]);
+                    ascii -= pobierz();                                                     //przesuń każdy znak w tablicy o tę liczbę znaków aby odkodować
+                    char c = Convert.ToChar(ascii);
+                    gotowe += c;
+                }
+
+                richTextBox1.Text = gotowe;                                                //umieść wynik deszyfrowania w tekstboxie
+            }
+            else
+                return;
+        }
+
+
+
+        private void btnCzysc_Click(object sender, EventArgs e)
+        {
+            textBoxSciezka.Text = "";
+            richTextBox1.Text = "";
+            textBoxZapis.Text = "";
+        }
+
+
+
+
+        private void btnZamknij_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+        async void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //  ne pozwala na przecinek 
+            if (e.KeyChar == ',' && textBox1.SelectionStart == 0)
+                e.Handled = true;
+
+            // pozwala na wprowadzanie tylko liczb
+            if (char.IsDigit(e.KeyChar) || e.KeyChar == ',' || e.KeyChar == (char)Keys.Back)
+                base.OnKeyPress(e);
+            else
+                e.Handled = true;
+
+            //po czasie 200ms wykonuje polecenie
+            await Task.Delay(200);
+            try
+            {
+                if (this.textBox1.Text != "")
+                {
+                    btnOdczyt.Enabled = true;
+                    btnZapisz.Enabled = true;
+                }
+                else
+                {
+                    btnOdczyt.Enabled = false;
+                    btnZapisz.Enabled = false;
+                }
+            }
+            catch
+            {
+                //tu nic nie ma
+            }
+        }
+
+
+
+        private static void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+                //textBoxZapis.InvokeIfRequired(c => c.Text += e.Data);
+            }
+        }
+
+        private void buttonSzukaj_Click(object sender, EventArgs e)
+        {
+
+            System.String S = "Hello World";
+
+            //textBoxSzukaj.Text = 
+
+        }
+    }
+}
